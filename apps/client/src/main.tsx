1cb5fe340ad7459dc9c5aa6ebb8faa984cc70f8c
aayush <aayushlad058@gmail.com>
2024-08-20 18:47:42 +0530

    env removed from client

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,8 +3,7 @@ import App from "./App.tsx";
 import "./index.css";
 import axios from "axios";
 import { Toaster } from "react-hot-toast";
-import "dotenv/config";
-axios.defaults.baseURL = process.env.API_URL || "http://localhost:8080";
+axios.defaults.baseURL = "http://localhost:8080";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<>
