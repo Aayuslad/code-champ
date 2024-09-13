351af9e29bd8cb28f09763189b9c0d61fb8403e9
Purvish Joshi <joshipurvish1904@gmail.com>
2024-09-13 21:18:28 +0530

    updates the signup page

---
+++ apps/client/src/components/footers/AuthFooter.tsx
@@ -0,0 +1,13 @@
+const AuthFooter = () => {
+    return (
+      <>
+        <footer className="flex justify-center items-center p-1 mt-auto bg-light200 dark:bg-dark200 dark:text-white border-t border-gray-500">
+          <a href="" className="text-sm text-gray-600">
+            © 2024 CodeChamp. All rights reserved.
+          </a>
+        </footer>
+      </>
+    );
+  };
+  
+  export default AuthFooter;
