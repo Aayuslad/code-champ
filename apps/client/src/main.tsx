f8fa9ff69c90ef553a079653a2a5f8e5e48560f0
AayushLad <aayush.lad@roimaint.com>
2026-06-12 10:23:22 +0530

    chore: update packages, update server url

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -6,7 +6,8 @@ import { Toaster } from "react-hot-toast";
 axios.defaults.withCredentials = true;
 // axios.defaults.baseURL = "https://backend.code-champ.xyz";
 // axios.defaults.baseURL = "https://code-champ-server.vercel.app";
-axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.baseURL = "https://server.codechamp.online";
+// axios.defaults.baseURL = "http://localhost:8080";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<>
